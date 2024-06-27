ea99d8d5cd410b9026077c68a26fa726256d257c
Raza Muhammad <waistband161@gmail.com>
2024-06-28 02:13:04 +0500

    leftbar working

--- src/components/leftBar/LeftBar.jsx
+++ src/components/leftBar/LeftBar.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import './LeftBar.scss'
+import { Link } from 'react-router-dom'
+import Friends from "../../assets/1.png";
+import Groups from "../../assets/2.png";
+import Market from "../../assets/3.png";
+import Watch from "../../assets/4.png";
+import Memories from "../../assets/5.png";
+import Events from "../../assets/6.png";
+import Gaming from "../../assets/7.png";
+import Gallery from "../../assets/8.png";
+import Videos from "../../assets/9.png";
+import Messages from "../../assets/10.png";
+import Tutorials from "../../assets/11.png";
+import Courses from "../../assets/12.png";
+import Fund from "../../assets/13.png";
 
 const LeftBar = () => {
   return (
-    <div>
-      <h1>LeftBar</h1>
+    <div className='leftBar'>
+      <div className="container">
+        <div className="menu">
+          <Link to ="/" style={{textDecoration:"none"}}><div className="user">
+            {/* <img src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" /> */}
+            <span>John</span>
+          </div></Link>
+          <div className="item">
+            <img src={Friends} alt="" />
+            <span>Friends</span>
+          </div>
+        </div>
+      </div>
+
+      
+      
     </div>
   )
 }
