73f75a8f49d3af6791e03dce5a83c5ef1cc7dec6
RazaMuhammad5758 <waistband161@gmail.com>
2024-07-04 02:37:07 +0500

    profile work start

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -12,7 +12,49 @@ import MoreVertIcon from "@mui/icons-material/MoreVert";
 const Profile = () => {
   return (
     <div className="profile">
-      <h1>Profile</h1>
+      <div className="images">
+        <img src="https://images.pexels.com/photos/13440765/pexels-photo-13440765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" className='cover' />
+        <img src="https://images.pexels.com/photos/14028501/pexels-photo-14028501.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" className='profilePic' />
+      </div>
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+          <a href="http://facebook.com">
+              <FacebookTwoToneIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <InstagramIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <TwitterIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <LinkedInIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <PinterestIcon fontSize="large" />
+            </a>
+          </div>
+          <div className="centre">
+            <span>Jahn Doe</span>
+            <div className="info">
+              <div className="item">
+                <PlaceIcon/>
+                <span>USA</span>
+              </div>
+              <div className="item">
+                <LanguageIcon/>
+                <span>razamuhammad</span>
+              </div>
+              <button>Follow</button>
+            </div>
+          </div>
+          <div className="right">
+            <EmailOutlinedIcon/>
+            <MoreVertIcon/>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
