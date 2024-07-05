d38a8c17e268c1c5777b7a807d3844b9a2538d3f
RazaMuhammad5758 <waistband161@gmail.com>
2024-07-06 00:29:54 +0500

    profile sec done

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -8,52 +8,63 @@ import PlaceIcon from "@mui/icons-material/Place";
 import LanguageIcon from "@mui/icons-material/Language";
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Post from "../../posts/Post"
+
 
 const Profile = () => {
   return (
     <div className="profile">
       <div className="images">
-        <img src="https://images.pexels.com/photos/13440765/pexels-photo-13440765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" className='cover' />
-        <img src="https://images.pexels.com/photos/14028501/pexels-photo-14028501.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" className='profilePic' />
+        <img
+          src="https://images.pexels.com/photos/13440765/pexels-photo-13440765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+          className="cover"
+        />
+        <img
+          src="https://images.pexels.com/photos/14028501/pexels-photo-14028501.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+          alt=""
+          className="profilePic"
+        />
       </div>
       <div className="profileContainer">
         <div className="uInfo">
           <div className="left">
-          <a href="http://facebook.com">
-              <FacebookTwoToneIcon fontSize="large" />
+            <a href="http://facebook.com">
+              <FacebookTwoToneIcon  />
             </a>
             <a href="http://facebook.com">
-              <InstagramIcon fontSize="large" />
+              <InstagramIcon />
             </a>
             <a href="http://facebook.com">
-              <TwitterIcon fontSize="large" />
+              <TwitterIcon  />
             </a>
             <a href="http://facebook.com">
-              <LinkedInIcon fontSize="large" />
+              <LinkedInIcon  />
             </a>
             <a href="http://facebook.com">
-              <PinterestIcon fontSize="large" />
+              <PinterestIcon />
             </a>
           </div>
           <div className="center">
-            <span>Jahn Doe</span>
+            <span>Jane Doe</span>
             <div className="info">
               <div className="item">
-                <PlaceIcon/>
+                <PlaceIcon />
                 <span>USA</span>
               </div>
               <div className="item">
-                <LanguageIcon/>
-                <span>razamuhammad</span>
+                <LanguageIcon />
+                <span>lama.dev</span>
               </div>
-              <button>Follow</button>
             </div>
+            <button>follow</button>
           </div>
           <div className="right">
-            <EmailOutlinedIcon/>
-            <MoreVertIcon/>
+            <EmailOutlinedIcon />
+            <MoreVertIcon />
           </div>
         </div>
+      <Post/>
       </div>
     </div>
   )
