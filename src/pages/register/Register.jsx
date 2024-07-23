0cd7d6850244a04b50ad17e9dfb03017bb8be713
RazaMuhammad5758 <waistband161@gmail.com>
2024-07-23 23:47:10 +0500

    regster api

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,60 +1,81 @@
-import { Link } from "react-router-dom"
-import "./Register.scss"
-import { useState } from "react"
+import { Link } from "react-router-dom";
+import "./Register.scss";
+import { useState } from "react";
 import axios from "axios";
 
 const Register = () => {
-
   const [inputs, setInputs] = useState({
-      username:"",
-      email:"",
-      password:"",
-      name:""
-
+    username: "",
+    email: "",
+    password: "",
+    name: "",
   });
   const [err, setErr] = useState(null);
 
-  const handleChange = (e)=>{
-    setInputs((prev)=>({...prev, [e.target.name]:e.target.value}));
-
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
-  const handleClick = async e=>{
-    e.preventDefault()
+  const handleClick = async (e) => {
+    e.preventDefault();
 
-    try{
-      await axios.post("http://localhost:8800/api/auth/register", inputs)
+    try {
+      await axios.post("http://localhost:8800/api/auth/register", inputs);
+    } catch (error) {
+      setErr(error.response?.data || "An error occurred");
     }
-    catch{
-      setErr(err.response.data);
-    }
-  }
+  };
+
   console.log(err);
 
   return (
     <div className="register">
       <div className="card">
-      
         <div className="left">
-            <h1>Friend Book.</h1>
-            <p>Connect with friends and the world around you on Hellow World. Dive into a vibrant community where sharing, discovering, and staying connected is effortless and fun.</p>
-            <span>Do you have an account?</span>
-            <Link to="/login"><button>Login</button></Link> 
+          <h1>Friend Book.</h1>
+          <p>
+            Connect with friends and the world around you on Hellow World. Dive
+            into a vibrant community where sharing, discovering, and staying
+            connected is effortless and fun.
+          </p>
+          <span>Do you have an account?</span>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
         </div>
         <div className="right">
-            <h1>Register</h1>
-            <form>
-                <input type="text" placeholder="Username" name="username" onChange={handleChange}/>
-                <input type="email" placeholder="Email" name="email" onChange={handleChange}/>
-                <input type="password" placeholder="Password" name="password" onChange={handleChange}/>
-                <input type="text" placeholder="Name" name="name" onChange={handleChange}/>
-                <button>Register</button>
-            </form>
+          <h1>Register</h1>
+          <form>
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              onChange={handleChange}
+            />
+            <button onClick={handleClick}>Register</button>
+          </form>
         </div>
-        
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
